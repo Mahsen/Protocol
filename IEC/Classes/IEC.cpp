1afefaf7c49cc136d08ad6f2afcb14da164d5d25
Mahsen <not.select@gmail.com>
2023-05-17 18:46:30 +0330

    Edit IEC class

--- IEC/Classes/IEC.cpp
+++ IEC/Classes/IEC.cpp
@@ -38,6 +38,18 @@
     Nothing
 */
 /************************************************** Functions *********************************************************/
+uint8_t IEC::GetBCC  (uint8_t *Data, uint32_t Length)
+{
+	uint8_t bcc = 0;
+
+	for(uint32_t Index=1; Index<Length; Index++)
+	{
+		bcc ^= Data[Index];
+	}
+
+	return bcc;
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
 /* Set address of device that use in connection */
 Status<IEC::Messages>* ClientIEC::SetAddress(char *Address) {
     /* Disconnect if current address diffrent with old address */
@@ -51,11 +63,21 @@ Status<IEC::Messages>* ClientIEC::SetAddress(char *Address) {
     return &status;
 }  
 /*--------------------------------------------------------------------------------------------------------------------*/
+/* Set mode of device that use in connection */
+Status<IEC::Messages>* ClientIEC::SetMode(char Mode) {
+    /* Disconnect if current mode diffrent with old mode */
+    if(Mode != _Mode) {
+            DisConnect();
+    }
+    /* Replace Mode */
+    _Mode = Mode;
+
+    status.Set(Messages::Success);
+    return &status;
+}  
+/*--------------------------------------------------------------------------------------------------------------------*/
 /* Command to connect IEC */
 Status<IEC::Messages>* ClientIEC::Connect() {
-
-    uint8_t Data[1024];
-    uint32_t Length=0;
 
     if(state.Get() == States::Connected) {
         status.Set(Messages::Success);
@@ -75,10 +97,15 @@ Status<IEC::Messages>* ClientIEC::Connect() {
     state.Set(States::Inited);
 
     state.Set(States::Connecting);
-    sprintf((char*)Data, "/?%s!\r\n", _Address);
-    _Media->Send(Data, strlen((char*)Data));
+    sprintf((char*)_Data, "/?%s!\r\n", _Address);
+    if(!_Media->Send(_Data, strlen((char*)_Data))) {
+        status.Set(Messages::Fault_Media);
+        return &status;
+    }
 
-    if(!_Media->Receive(&Data[Length], &Length)) {
+    memset(_Data, 0, sizeof(_Data));
+    _Length = 0;
+    if(!_Media->Receive(&_Data[_Length], &_Length)) {
         if(DisConnect()->Get() == Messages::Success)
         {
             status.Set(Messages::Fault_TimeOut);
@@ -86,15 +113,81 @@ Status<IEC::Messages>* ClientIEC::Connect() {
         return &status;
     } 
 
-    
+    char *Identification = strchr((char*)_Data, '/');
+    if(Identification == nullptr) {
+        status.Set(Messages::Fault_Data);
+        return &status;
+    }
+
+    if((Identification[4] - '0') > 9) {
+        status.Set(Messages::Fault_Speed);
+        return &status;
+    }
+
+    /* Change speed line */
+    _Wait(50);
+    if(!_Media->Update(IEC_Table_BoadRate[Identification[4] - '0'])) {
+        status.Set(Messages::Fault_Media);
+        return &status;
+    }
+    _Media->Clear();
+    _Wait(50);
+
+    sprintf((char*)_Data, "%c0%c%c\r\n", IEC_CHAR_ACK, Identification[4], _Mode);
+    if(!_Media->Send(_Data, strlen((char*)_Data))) {
+        status.Set(Messages::Fault_Media);
+        return &status;
+    }
+
+    memset(_Data, 0, sizeof(_Data));
+    _Length = 0;
+    if(!_Media->Receive(&_Data[_Length], &_Length)) {
+        if(DisConnect()->Get() == Messages::Success)
+        {
+            status.Set(Messages::Fault_TimeOut);
+        }
+        return &status;
+    } 
+    _Media->Clear();
+
     state.Set(States::Connected);
 
     status.Set(Messages::Success);
     return &status;
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
-/* Command to execute data to IEC */
-Status<IEC::Messages>* ClientIEC::Execute(char *Data, uint32_t Length) {
+/* Command to connect IEC with Address */
+Status<IEC::Messages>* ClientIEC::Connect(char *Address) {
+
+    if(SetAddress(Address)->Get() != Messages::Success) {
+        return &status;
+    }
+
+    return Connect();
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Command to connect IEC with address and mode */
+Status<IEC::Messages>* ClientIEC::Connect(char *Address, char Mode) {
+
+    if(SetMode(Mode)->Get() != Messages::Success) {
+        return &status;
+    }
+
+    return Connect(Address);
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Command to connect IEC with Address */
+Status<IEC::Messages>* ClientIEC::GetData(char* Data, uint32_t Length) {
+
+    Data = (char*)_Data;
+    Length = _Length;
+
+    status.Set(Messages::Success);
+    return &status;
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Command to execute data to IEC with callback */
+Status<IEC::Messages>* ClientIEC::Execute(char *Data, void (*CallBack)(char *Data, uint32_t Length)) {
     /* Send connect command if IEC is not connected */
     if(state.Get() != States::Connected) {
         if(Connect()->Get() != Messages::Success) {
@@ -105,12 +198,86 @@ Status<IEC::Messages>* ClientIEC::Execute(char *Data, uint32_t Length) {
     state.Set(States::Executeing);
     
     cout << Data << endl;
+    
+    uint32_t Length = 0;
+	char BCC;
+	char *pch, *etx;
+
+	pch = NULL;
+	etx = NULL;
+	
+	pch = strtok((char*)Data, IEC_CHAR_ETX);	
+	etx = pch;
+	
+	if(etx == NULL)
+	{
+		pch = strtok((char*)Data, IEC_CHAR_EOT);
+	}
+
+	while(pch)
+	{
+		Length = strlen(pch);
+
+		sprintf((char*)_Data, "%s%c", pch, (etx)?IEC_CHAR_ETX:IEC_CHAR_EOT);
+		BCC = GetBCC(_Data, ++Length);
+		_Data[Length++] = BCC;
+
+		SendReceive:
+
+        _Media->Clear();
+        if(!_Media->Send(_Data, strlen((char*)_Data))) {
+            status.Set(Messages::Fault_Media);
+            return &status;
+        }
+
+        memset(_Data, 0, sizeof(_Data));
+        _Length = 0;
+        if(!_Media->Receive(&_Data[_Length], &_Length)) {
+            if(DisConnect()->Get() == Messages::Success)
+            {
+                status.Set(Messages::Fault_TimeOut);
+            }
+            return &status;
+        } 
+
+		if(CallBack)
+		{
+			if(_Length)
+			{
+				CallBack((char*)_Data, (_Length-1));
+			}
+		}
+		
+		if((_Data[_Length-2] == (uint8_t)IEC_CHAR_EOT) || ((_Data[_Length-2] != (uint8_t)IEC_CHAR_ETX) && (_Data[_Length-1] == (uint8_t)IEC_CHAR_EOT)))
+		{
+			sprintf((char*)_Data, "%c", IEC_CHAR_ACK);
+			Length = 1;
+			
+			goto SendReceive;
+		}		
+		
+		pch = NULL;
+		etx = NULL;
+		
+		pch = strtok(NULL, IEC_CHAR_ETX);	
+		etx = pch;
+		
+		if(etx == NULL)
+		{
+			pch = strtok(NULL, IEC_CHAR_EOT);
+		}
+
+	}
 
     state.Set(States::Connected);
 
-
     status.Set(Messages::Success);
     return &status;
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Command to execute data to IEC */
+Status<IEC::Messages>* ClientIEC::Execute(char *Data) {
+    return Execute(Data, nullptr);
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Command to disconnect IEC */
@@ -121,11 +288,14 @@ Status<IEC::Messages>* ClientIEC::DisConnect() {
         return &status;
     }
 
-    if(state.Get() == States::Connected) {
-        _Media->Send((uint8_t*)"exit\r\n", 6);
-    }
-
     state.Set(States::DisConnecting);
+    if(state.Get() == States::Connected) {
+        sprintf((char*)_Data, "%cB0%cq", IEC_CHAR_SOH, IEC_CHAR_ETX);
+        if(!_Media->Send(_Data, strlen((char*)_Data))) {
+            status.Set(Messages::Fault_Media);
+            return &status;
+        }
+    }
     if(!_Media->Close()) {
         status.Set(Messages::Fault_Media);
         return &status;
--- IEC/Classes/IEC.hpp
+++ IEC/Classes/IEC.hpp
@@ -31,22 +31,26 @@
 /* State class is there */
 #include "../Interface/State.hpp"
 /************************************************** Defineds **********************************************************/
-/*
-    Nothing
-*/
+#define IEC_CHAR_SOH ""
+#define IEC_CHAR_STX ""
+#define IEC_CHAR_ETX ""
+#define IEC_CHAR_EOT ""
+#define IEC_CHAR_ACK ""
+#define IEC_CHAR_NAK ""
 /************************************************** Names *************************************************************/
 /* Using std */
 using namespace std;
 /************************************************** Variables *********************************************************/
-/*
-    Nothing
-*/
+const uint32_t IEC_Table_BoadRate[] = {305, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200};
 /************************************************** Opjects ***********************************************************/
 class IEC {
     public:
         Media* _Media;
         void (*_Wait)(uint32_t);
         char _Address[16];
+        char _Mode;
+        uint8_t _Data[1024];
+        uint32_t _Length;
 
         /* All possible states */
         enum class States {
@@ -66,10 +70,12 @@ class IEC {
             Success,
             Fault,
             Fault_Media,
-            Fault_TimeOut
+            Fault_TimeOut,
+            Fault_Data,
+            Fault_Speed
         };
         Status<Messages> status;
-        const char* Status2String[5] = {"Success", "Fault", "Fault_Media", "Fault_TimeOut"};
+        const char* Status2String[6] = {"Success", "Fault", "Fault_Media", "Fault_TimeOut", "Fault_Data", "Fault_Speed"};
 
         void ShowStatus() {
             cout << "ShowStatus(" << Status2String[(int)status.Get()] << ")" << endl;
@@ -79,15 +85,23 @@ class IEC {
             state.Set(States::UnInited);
             _Media = P_Media;
             _Wait = P_Wait;
+            memset(_Data, 0, sizeof(_Data));
+            _Length = 0;
         };
+        uint8_t GetBCC(uint8_t *Data, uint32_t Length);
 };
 /*--------------------------------------------------------------------------------------------------------------------*/
 class ClientIEC : private IEC {
     public:
-        ClientIEC(Media* P_Media, void (*P_Wait)(uint32_t)) : IEC(P_Media, P_Wait) {};
+        ClientIEC(Media* P_Media, void (*P_Wait)(uint32_t)) : IEC(P_Media, P_Wait) { SetAddress(""); SetMode('0'); };
 
         Status<Messages>* SetAddress(char *Address);
+        Status<Messages>* SetMode(char Mode);
+        Status<Messages>* GetData(char* Data, uint32_t Length);
         Status<Messages>* Connect();
+        Status<Messages>* Connect(char *Address);
+        Status<Messages>* Connect(char *Address, char Mode);
+        Status<Messages>* Execute(char *Data, uint32_t Length, void (*CallBack)(char *Data, uint32_t Length));
         Status<Messages>* Execute(char *Data, uint32_t Length);
         Status<Messages>* DisConnect();
 };
